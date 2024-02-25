17be63060a48ecdd4496cb93b2bb7bb260939888
andriadolidze <dolidze545@gmail.com>
2024-02-24 23:10:57 -0500

    first commit

---
+++ Day 01/homeworks/lesson1.py
@@ -0,0 +1,34 @@
+name = "andria"
+# არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "andria" არის ცვლადისთვის მნიშვნელობა
+
+surname = "dolidze"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "andia" # ეს არის str (string) ტიპის ცვლადი
+age = "14" # ეს არის int ( integer ) მთელი რიცხვი
+heigh = 165 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True  #True ან False
+is_ugly = False  #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+print(name +" "+surname)
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+print(name + age)
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(heigh))
+print(type(knows_programming))
+
+print(name + " " + str(age))
+
---
+++ Day 02/homeworks/lesson2.py
@@ -0,0 +1,2 @@
+print("me gavicani: nikoloz smith da mate giorgelashvili")
+print("me goashi momewona: megobruli garemo gamgebiani mentorebi batoni nikas axsnili")
