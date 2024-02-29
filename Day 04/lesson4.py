f82b639a4c78d4f1802769aa30c3e0938ddb82e0
andriadolidze <dolidze545@gmail.com>
2024-02-29 14:45:01 -0500

    commit

---
+++ Day 04/lesson4.py
@@ -0,0 +1,44 @@
+# დავპრინტოდ 2 რიცხვის ჯამი
+number1 = input("please enter number: ")
+number2 = input("please enter number: ")
+print("sum is" + " " + str(int(number1) + int(number2)))
+#დავასრულე 2 რიცხვის ჯამის კოდი
+
+#დროში მოგზაურობის კოდი
+name = input("please enter your name: ")
+age = input("please enter your age: ")
+time_travel = input("how many years in to the future do you want to time travel: ")
+print("your age in the future: " + " " + str(int(age) + int(time_travel)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
