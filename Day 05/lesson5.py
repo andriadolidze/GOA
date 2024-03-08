e274dd5e4a8824e41ad760f6a36f410b1aa6cb31
andriadolidze <dolidze545@gmail.com>
2024-03-08 14:49:50 -0500

    commit

---
+++ Day 05/lesson5.py
@@ -0,0 +1,17 @@
+num1 = int(input("please enter firts number; "))
+num2 = int(input("please enter firts number; "))
+
+print(num1 / num2)
+
+num1 = int(input("please enter firts number; "))
+num2 = int(input("please enter firts number; "))
+
+print(num1 // num2)
+
+num1 = int(input("please enter firts number; "))
+num2 = int(input("please enter firts number; "))
+
+print(num1 % num2)
+
+
+
