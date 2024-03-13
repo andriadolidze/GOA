1e30c3f2b517f482d72e650d9968a3f8340e042a
andriadolidze <dolidze545@gmail.com>
2024-03-13 11:54:52 -0400

    commit

---
+++ Day 06/lesson6.py
@@ -0,0 +1,77 @@
+num1 = int(input("please enter your number: "))
+num2 = float(num1)
+print(num1 == num2)
+#ეს იყო პირველი დავალება
+
+print(5 > 2)
+print(7 > 5)
+print(11 > 9)
+print(23 > 21)
+print(1 > 0)
+
+print(7 >= 2)
+print(8 >= 3)
+print(19 >= 19)
+print(40 >= 40)
+print(18 >= 2)
+
+print(2 < 7)
+print(7 < 9)
+print(6 < 9)
+print(19 < 20)
+print(39 < 40)
+
+print(5 <= 7)
+print(7 <= 9)
+print(9 <= 11)
+print(11 <= 13)
+print(13 <= 15)
+
+print(40 == 40)
+print(39 == 39)
+print(38 == 38)
+print(37 == 37)
+print(36 == 36)
+
+print(7 != 8)
+print(10 != 11)
+print(9 != 10)
+print(12 != 13)
+print(13 != 14)
+
+number1 = int(input("please enter firts number: "))
+number2 = int(input("please enter second number: "))
+
+print(number1 > number2)
+print(number1 >= number2)
+print(number1 < number2)
+print(number1 <= number2)
+print(number1 == number2)
+print(number1 != number2)
+
+number1 = int(input("please enter firts number: "))
+number2 = number1 + 10
+
+print(number1 > number2)
+print(number1 >= number2)
+print(number1 < number2)
+print(number1 <= number2)
+print(number1 == number2)
+print(number1 != number2)
+
+number1 = int(input("please enter firts number: "))
+number2 = float(input("please enter second number:  "))
+
+print(number1 == number2)
+print(number1 != number2)
+
+
+
+
+
+
+
+
+
+
+
